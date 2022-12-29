5cf6bf668651967fab2f6b8078c549725986c733
Vojta <vojta.salajka@seznam.cz>
2022-12-29 23:05:00 +0100

    Updated example to current bgfx (2022-12-29)

--- main.cpp
+++ main.cpp
@@ -1,60 +1,30 @@
-#include <bx/math.h>
-#include <bx/bx.h>
-#include <bx/mutex.h>
-#include <bx/thread.h>
-#include <bgfx/bgfx.h>
 #include <bgfx/platform.h>
-#include <bgfx/embedded_shader.h>
+
+#if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
+#	if ENTRY_CONFIG_USE_WAYLAND
+#		include <wayland-egl.h>
+#	endif
+#elif BX_PLATFORM_WINDOWS
+#	define SDL_MAIN_HANDLED
+#endif
+
+#include <bx/os.h>
 
 #include <SDL2/SDL.h>
+
+BX_PRAGMA_DIAGNOSTIC_PUSH()
+BX_PRAGMA_DIAGNOSTIC_IGNORED_CLANG("-Wextern-c-compat")
 #include <SDL2/SDL_syswm.h>
+BX_PRAGMA_DIAGNOSTIC_POP()
 
-#include "vs_cubes.bin.h"
-#include "fs_cubes.bin.h"
+#if defined(None) // X11 defines this...
+#	undef None
+#endif // defined(None)
 
-static bgfx::PlatformData sdlPlatformData(SDL_Window* _window)
-{
-    SDL_SysWMinfo wmi;
-    SDL_VERSION(&wmi.version);        
-    bgfx::PlatformData platformData;
-    platformData.ndt = nullptr;
-    platformData.nwh = nullptr;
-    platformData.context = nullptr;
-    platformData.backBuffer = nullptr;
-    platformData.backBufferDS = nullptr;
-    if (!SDL_GetWindowWMInfo(_window, &wmi))
-    {        
-        return platformData;
-    }
-#	if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
-#		if ENTRY_CONFIG_USE_WAYLAND
-    wl_egl_window *win_impl = (wl_egl_window*)SDL_GetWindowData(_window, "wl_egl_window");
-    if(!win_impl)
-    {
-        int width, height;
-        SDL_GetWindowSize(_window, &width, &height);
-        struct wl_surface* surface = wmi.info.wl.surface;
-        if(!surface)
-        {
-            return platformData;
-        }
-        win_impl = wl_egl_window_create(surface, width, height);
-        SDL_SetWindowData(_window, "wl_egl_window", win_impl);
-    }
-    platformData.nwh = (void*)(uintptr_t)win_impl;
-#		else
-    platformData.nwh = (void*)wmi.info.x11.window;
-    platformData.ndt = wmi.info.x11.display;
-#		endif
-#	elif BX_PLATFORM_OSX || BX_PLATFORM_IOS
-    platformData.nwh = wmi.info.cocoa.window;
-#	elif BX_PLATFORM_WINDOWS
-    platformData.nwh = wmi.info.win.window;
-#   elif BX_PLATFORM_ANDROID
-    platformData.nwh = wmi.info.android.window;
-#	endif // BX_PLATFORM_
-    return platformData;
-}
+//#include <bx/mutex.h>
+//#include <bx/thread.h>
+//#include <bx/handlealloc.h>
+#include <bx/readerwriter.h>
 
 struct PosColorVertex
 {
@@ -66,10 +36,10 @@ struct PosColorVertex
     static void init()
     {
         ms_layout
-        .begin()
-        .add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
-        .add(bgfx::Attrib::Color0,   4, bgfx::AttribType::Uint8, true)
-        .end();
+            .begin()
+            .add(bgfx::Attrib::Position, 3, bgfx::AttribType::Float)
+            .add(bgfx::Attrib::Color0,   4, bgfx::AttribType::Uint8, true)
+            .end();
     };
 
     static bgfx::VertexLayout ms_layout;
@@ -105,93 +75,328 @@ static const uint16_t s_cubeTriList[] =
     6, 3, 7,
 };
 
-bgfx::VertexBufferHandle m_vbh;
-bgfx::IndexBufferHandle m_ibh;
-
-bgfx::ProgramHandle m_program;
-
-int main(int, char**)
+static const uint16_t s_cubeTriStrip[] =
 {
-    SDL_InitSubSystem(SDL_INIT_VIDEO);
+    0, 1, 2,
+    3,
+    7,
+    1,
+    5,
+    0,
+    4,
+    2,
+    6,
+    7,
+    4,
+    5,
+};
 
-    const int m_width = 800;
-    const int m_height = 600;
+static const uint16_t s_cubeLineList[] =
+{
+    0, 1,
+    0, 2,
+    0, 4,
+    1, 3,
+    1, 5,
+    2, 3,
+    2, 6,
+    3, 7,
+    4, 5,
+    4, 6,
+    5, 7,
+    6, 7,
+};
 
-    SDL_Window* window = nullptr;
+static const uint16_t s_cubeLineStrip[] =
+{
+    0, 2, 3, 1, 5, 7, 6, 4,
+    0, 2, 6, 4, 5, 7, 3, 1,
+    0,
+};
 
-    // sdl2
+static const uint16_t s_cubePoints[] =
+{
+    0, 1, 2, 3, 4, 5, 6, 7
+};
+
+static const char* s_ptNames[]
+{
+    "Triangle List",
+    "Triangle Strip",
+    "Lines",
+    "Line Strip",
+    "Points",
+};
+
+static const uint64_t s_ptState[]
+{
+    UINT64_C(0),
+    BGFX_STATE_PT_TRISTRIP,
+    BGFX_STATE_PT_LINES,
+    BGFX_STATE_PT_LINESTRIP,
+    BGFX_STATE_PT_POINTS,
+};
+BX_STATIC_ASSERT(BX_COUNTOF(s_ptState) == BX_COUNTOF(s_ptNames) );
+
+SDL_Window* g_window = nullptr;
+
+static void* sdlNativeWindowHandle(SDL_Window* _window)
+{
+    SDL_SysWMinfo wmi;
+    SDL_VERSION(&wmi.version);
+    if (!SDL_GetWindowWMInfo(_window, &wmi) )
     {
-
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-
-        window = SDL_CreateWindow(
-                     "bgfx SDL2",
-                     SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                     m_width, m_height,
-                     0
-                 );
+        return NULL;
     }
 
-    // bgfx
+#	if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
+#		if ENTRY_CONFIG_USE_WAYLAND
+    wl_egl_window *win_impl = (wl_egl_window*)SDL_GetWindowData(_window, "wl_egl_window");
+    if(!win_impl)
     {
-        bgfx::setPlatformData(sdlPlatformData(window));
+        int width, height;
+        SDL_GetWindowSize(_window, &width, &height);
+        struct wl_surface* surface = wmi.info.wl.surface;
+        if(!surface)
+            return nullptr;
+        win_impl = wl_egl_window_create(surface, width, height);
+        SDL_SetWindowData(_window, "wl_egl_window", win_impl);
+    }
+    return (void*)(uintptr_t)win_impl;
+#		else
+    return (void*)wmi.info.x11.window;
+#		endif
+#	elif BX_PLATFORM_OSX || BX_PLATFORM_IOS
+    return wmi.info.cocoa.window;
+#	elif BX_PLATFORM_WINDOWS
+    return wmi.info.win.window;
+#   elif BX_PLATFORM_ANDROID
+    return wmi.info.android.window;
+#	endif // BX_PLATFORM_
+}
 
-        // prevent creation of a renderer thread
-        bgfx::renderFrame();
+void* getNativeWindowHandle()
+{
+    return sdlNativeWindowHandle(g_window);
+}
+
+void* getNativeDisplayHandle()
+{
+    SDL_SysWMinfo wmi;
+    SDL_VERSION(&wmi.version);
+    if (!SDL_GetWindowWMInfo(g_window, &wmi) )
+    {
+        return NULL;
+    }
+
+#	if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
+#		if ENTRY_CONFIG_USE_WAYLAND
+    return wmi.info.wl.display;
+#		else
+    return wmi.info.x11.display;
+#		endif // ENTRY_CONFIG_USE_WAYLAND
+#	else
+    return NULL;
+#	endif // BX_PLATFORM_*
+}
+
+static const bgfx::Memory* loadMem(const char* _filePath)
+{
+    FILE* fin = fopen(_filePath, "rb");
+
+    if (fin == nullptr)
+    {
+        printf("%s not found\n", _filePath);
+        fflush(stdout);
+        return NULL;
+    }
+
+    long file_size;
+
+    if (fseek(fin, 0 , SEEK_END) != 0) {
+      /* Handle error */
+    }
+
+    file_size = ftell(fin);
+    if (file_size == -1) {
+      /* Handle error */
+    }
+
+    uint32_t size = file_size;
+    const bgfx::Memory* mem = bgfx::alloc(size+1);
+
+    rewind(fin);
+
+    fread(mem->data, 1, size, fin);
+
+    fclose(fin);
+
+    mem->data[mem->size-1] = '\0';
+
+    return mem;
+}
+
+bgfx::ShaderHandle loadShader(const char* _name)
+{
+    char filePath[512];
+
+    const char* shaderPath = "???";
+
+    switch (bgfx::getRendererType() )
+    {
+    case bgfx::RendererType::Noop:
+    case bgfx::RendererType::Direct3D9:  shaderPath = "bgfx/examples/runtime/shaders/dx9/";   break;
+    case bgfx::RendererType::Direct3D11:
+    case bgfx::RendererType::Direct3D12: shaderPath = "bgfx/examples/runtime/shaders/dx11/";  break;
+    case bgfx::RendererType::Agc:
+    case bgfx::RendererType::Gnm:        shaderPath = "bgfx/examples/runtime/shaders/pssl/";  break;
+    case bgfx::RendererType::Metal:      shaderPath = "bgfx/examples/runtime/shaders/metal/"; break;
+    case bgfx::RendererType::Nvn:        shaderPath = "bgfx/examples/runtime/shaders/nvn/";   break;
+    case bgfx::RendererType::OpenGL:     shaderPath = "bgfx/examples/runtime/shaders/glsl/";  break;
+    case bgfx::RendererType::OpenGLES:   shaderPath = "bgfx/examples/runtime/shaders/essl/";  break;
+    case bgfx::RendererType::Vulkan:     shaderPath = "bgfx/examples/runtime/shaders/spirv/"; break;
+    case bgfx::RendererType::WebGPU:     shaderPath = "bgfx/examples/runtime/shaders/spirv/"; break;
+
+    case bgfx::RendererType::Count:
+        BX_ASSERT(false, "You should not be here!");
+        break;
+    }
+
+    bx::strCopy(filePath, BX_COUNTOF(filePath), shaderPath);
+    bx::strCat(filePath, BX_COUNTOF(filePath), _name);
+    bx::strCat(filePath, BX_COUNTOF(filePath), ".bin");
+
+    bgfx::ShaderHandle handle = bgfx::createShader(loadMem(filePath) );
+    bgfx::setName(handle, _name);
+
+    return handle;
+}
+
+bgfx::ProgramHandle loadProgram(const char* _vsName, const char* _fsName)
+{
+    bgfx::ShaderHandle vsh = loadShader(_vsName);
+    bgfx::ShaderHandle fsh = BGFX_INVALID_HANDLE;
+    if (NULL != _fsName)
+    {
+        fsh = loadShader(_fsName);
+    }
+
+    return bgfx::createProgram(vsh, fsh, true /* destroy shaders when program is destroyed */);
+}
+
+class ExampleCubes
+{
+public:
+    ExampleCubes()
+        : m_pt(0)
+        , m_r(true)
+        , m_g(true)
+        , m_b(true)
+        , m_a(true)
+    {
+    }
+
+    void init(int32_t _argc, const char* const* _argv, uint32_t _width, uint32_t _height)
+    {
+        //Args args(_argc, _argv);
+
+        m_width  = _width;
+        m_height = _height;
+        m_debug  = BGFX_DEBUG_NONE;
+        m_reset  = BGFX_RESET_VSYNC;
 
         bgfx::Init init;
-        init.type =
-            //bgfx::RendererType::Vulkan;
-            bgfx::RendererType::OpenGL;
-        init.resolution.width = m_width;
+        //init.type     = args.m_type;
+        init.type     = bgfx::RendererType::OpenGL;
+        //init.type     = bgfx::RendererType::Vulkan;
+        init.vendorId = BGFX_PCI_ID_NONE; //args.m_pciId;
+        init.platformData.nwh  = getNativeWindowHandle();
+        init.platformData.ndt  = getNativeDisplayHandle();
+        init.resolution.width  = m_width;
         init.resolution.height = m_height;
-        init.resolution.reset = BGFX_RESET_VSYNC;
+        init.resolution.reset  = m_reset;
         bgfx::init(init);
-        bgfx::setViewRect(0, 0, 0, uint16_t(m_width), uint16_t(m_height) );
-        //bgfx::setScissor()
 
-        bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x000088FF, 1.f, 0);
+        // Enable debug text.
+        bgfx::setDebug(m_debug);
 
-        static const bgfx::EmbeddedShader s_embeddedShaders[] =
-        {
-            BGFX_EMBEDDED_SHADER(vs_cubes),
-            BGFX_EMBEDDED_SHADER(fs_cubes),
-
-            BGFX_EMBEDDED_SHADER_END()
-        };
-
-        bgfx::RendererType::Enum type = bgfx::getRendererType();
-
-        bgfx::ShaderHandle vsh = bgfx::createEmbeddedShader(s_embeddedShaders, type, "vs_cubes");
-        bgfx::ShaderHandle fsh = bgfx::createEmbeddedShader(s_embeddedShaders, type, "fs_cubes");
-
-        if (vsh.idx == 65535 || fsh.idx == 65535)
-        {
-            exit(1);
-        }
-
-        // Create program from shaders.
-        m_program = bgfx::createProgram(vsh, fsh, true /* destroy shaders when program is destroyed */);
+        // Set view 0 clear state.
+        bgfx::setViewClear(0
+            , BGFX_CLEAR_COLOR|BGFX_CLEAR_DEPTH
+            , 0x303030ff
+            , 1.0f
+            , 0
+            );
 
         // Create vertex stream declaration.
         PosColorVertex::init();
 
         // Create static vertex buffer.
         m_vbh = bgfx::createVertexBuffer(
-                    // Static data can be passed with bgfx::makeRef
-                    bgfx::makeRef(s_cubeVertices, sizeof(s_cubeVertices) )
-                    , PosColorVertex::ms_layout
-                );
+            // Static data can be passed with bgfx::makeRef
+              bgfx::makeRef(s_cubeVertices, sizeof(s_cubeVertices) )
+            , PosColorVertex::ms_layout
+            );
 
         // Create static index buffer for triangle list rendering.
-        m_ibh = bgfx::createIndexBuffer(
-                    // Static data can be passed with bgfx::makeRef
-                    bgfx::makeRef(s_cubeTriList, sizeof(s_cubeTriList) )
-                );
+        m_ibh[0] = bgfx::createIndexBuffer(
+            // Static data can be passed with bgfx::makeRef
+            bgfx::makeRef(s_cubeTriList, sizeof(s_cubeTriList) )
+            );
+
+        // Create static index buffer for triangle strip rendering.
+        m_ibh[1] = bgfx::createIndexBuffer(
+            // Static data can be passed with bgfx::makeRef
+            bgfx::makeRef(s_cubeTriStrip, sizeof(s_cubeTriStrip) )
+            );
+
+        // Create static index buffer for line list rendering.
+        m_ibh[2] = bgfx::createIndexBuffer(
+            // Static data can be passed with bgfx::makeRef
+            bgfx::makeRef(s_cubeLineList, sizeof(s_cubeLineList) )
+            );
+
+        // Create static index buffer for line strip rendering.
+        m_ibh[3] = bgfx::createIndexBuffer(
+            // Static data can be passed with bgfx::makeRef
+            bgfx::makeRef(s_cubeLineStrip, sizeof(s_cubeLineStrip) )
+            );
+
+        // Create static index buffer for point list rendering.
+        m_ibh[4] = bgfx::createIndexBuffer(
+            // Static data can be passed with bgfx::makeRef
+            bgfx::makeRef(s_cubePoints, sizeof(s_cubePoints) )
+            );
+
+        // Create program from shaders.
+        m_program = loadProgram("vs_cubes", "fs_cubes");
+
+        m_timeOffset = SDL_GetTicks(); // SDL_TICKS_PASSED
+    }
+
+    virtual int shutdown()
+    {
+        // Cleanup.
+        for (uint32_t ii = 0; ii < BX_COUNTOF(m_ibh); ++ii)
+        {
+            bgfx::destroy(m_ibh[ii]);
+        }
+
+        bgfx::destroy(m_vbh);
+        bgfx::destroy(m_program);
+
+        // Shutdown bgfx.
+        bgfx::shutdown();
+
+        return 0;
+    }
+
+    void update()
+    {
+        float time = (float)( (SDL_GetTicks()-m_timeOffset)/double(1000) );
 
         const bx::Vec3 at  = { 0.0f, 0.0f,   0.0f };
-        const bx::Vec3 eye = { 0.0f, 0.0f, -5.0f };
+        const bx::Vec3 eye = { 0.0f, 0.0f, -35.0f };
 
         // Set view and projection matrix for view 0.
         {
@@ -205,73 +410,114 @@ int main(int, char**)
             // Set view 0 default viewport.
             bgfx::setViewRect(0, 0, 0, uint16_t(m_width), uint16_t(m_height) );
         }
+
+        // This dummy draw call is here to make sure that view 0 is cleared
+        // if no other draw calls are submitted to view 0.
+        bgfx::touch(0);
+
+        bgfx::IndexBufferHandle ibh = m_ibh[m_pt];
+        uint64_t state = 0
+            | (m_r ? BGFX_STATE_WRITE_R : 0)
+            | (m_g ? BGFX_STATE_WRITE_G : 0)
+            | (m_b ? BGFX_STATE_WRITE_B : 0)
+            | (m_a ? BGFX_STATE_WRITE_A : 0)
+            | BGFX_STATE_WRITE_Z
+            | BGFX_STATE_DEPTH_TEST_LESS
+            | BGFX_STATE_CULL_CW
+            | BGFX_STATE_MSAA
+            | s_ptState[m_pt]
+            ;
+
+        // Submit 11x11 cubes.
+        for (uint32_t yy = 0; yy < 11; ++yy)
+        {
+            for (uint32_t xx = 0; xx < 11; ++xx)
+            {
+                float mtx[16];
+                bx::mtxRotateXY(mtx, time + xx*0.21f, time + yy*0.37f);
+                mtx[12] = -15.0f + float(xx)*3.0f;
+                mtx[13] = -15.0f + float(yy)*3.0f;
+                mtx[14] = 0.0f;
+
+                // Set model matrix for rendering.
+                bgfx::setTransform(mtx);
+
+                // Set vertex and index buffer.
+                bgfx::setVertexBuffer(0, m_vbh);
+                bgfx::setIndexBuffer(ibh);
+
+                // Set render states.
+                bgfx::setState(state);
+
+                // Submit primitive for rendering to view 0.
+                bgfx::submit(0, m_program);
+            }
+        }
+
+        // Advance to next frame. Rendering thread will be kicked to
+        // process submitted rendering primitives.
+        bgfx::frame();
     }
 
-    float time = 0.f;
+    uint32_t m_width;
+    uint32_t m_height;
+    uint32_t m_debug;
+    uint32_t m_reset;
+    bgfx::VertexBufferHandle m_vbh;
+    bgfx::IndexBufferHandle m_ibh[BX_COUNTOF(s_ptState)];
+    bgfx::ProgramHandle m_program;
+    Uint32 m_timeOffset;
+    int32_t m_pt;
 
-    while (1)
+    bool m_r;
+    bool m_g;
+    bool m_b;
+    bool m_a;
+};
+
+int main(int argc, char** argv)
+{
+    SDL_Init(0
+        | SDL_INIT_GAMECONTROLLER
+        );
+
+    int m_width = 1280;
+    int m_height = 720;
+
+    g_window = SDL_CreateWindow("bgfx"
+                    , SDL_WINDOWPOS_UNDEFINED
+                    , SDL_WINDOWPOS_UNDEFINED
+                    , m_width
+                    , m_height
+                    , SDL_WINDOW_SHOWN
+                    );
+
+    bgfx::renderFrame();
+
+    ExampleCubes cubes;
+
+    cubes.init(argc, argv, m_width, m_height);
+
+    bool exit = false;
+    SDL_Event event;
+    while (!exit)
     {
-        // sdl events
-        SDL_Event event;
-        bool bQuit = false;
-        while (SDL_PollEvent(&event))
+        cubes.update();
+        //bgfx::renderFrame();
+
+        while (SDL_PollEvent(&event) )
         {
             switch (event.type)
             {
             case SDL_QUIT:
-                bQuit = true;
+                exit = true;
                 break;
             }
         }
-
-        if (bQuit)
-        {
-            break;
-        }
-
-        bgfx::touch(0);
-
-        uint64_t state = 0
-                         | BGFX_STATE_WRITE_R
-                         | BGFX_STATE_WRITE_G
-                         | BGFX_STATE_WRITE_B
-                         | BGFX_STATE_WRITE_A
-                         | BGFX_STATE_WRITE_Z
-                         | BGFX_STATE_DEPTH_TEST_LESS
-                         | BGFX_STATE_CULL_CW
-                         | BGFX_STATE_MSAA
-                         | 0
-                         ;
-
-        {
-            float mtx[16];
-            bx::mtxRotateY(mtx, time);
-
-            // Set model matrix for rendering.
-            bgfx::setTransform(mtx);
-
-            // Set vertex and index buffer.
-            bgfx::setVertexBuffer(0, m_vbh);
-            bgfx::setIndexBuffer(m_ibh);
-
-            // Set render states.
-            bgfx::setState(state);
-
-            // Submit primitive for rendering to view 0.
-            bgfx::submit(0, m_program);
-        }
-
-        bgfx::frame();
-
-        time += 0.05f;
     }
 
-    bgfx::destroy(m_vbh);
-    bgfx::destroy(m_ibh);
-    bgfx::destroy(m_program);
-
-    bgfx::shutdown();
+    cubes.shutdown();
+    SDL_DestroyWindow(g_window);
     SDL_Quit();
-
     return 0;
 }
